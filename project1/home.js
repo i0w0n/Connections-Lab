57148b81e43a1263750a497d979211bc45e5dd8c
i0w0n <71196191+i0w0n@users.noreply.github.com>
2023-10-05 03:41:44 +0800

    Add files via upload

--- project1/home.js
+++ project1/home.js
@@ -14,8 +14,8 @@ function setup(){
 }
 
 function draw(){
-    image(catimg, 950,50,250,250);
-    image(shibaimg, 300,100,150,150);
     background(255, 206, 107);
+    image(catimg, windowWidth*0.65,50,250,250);
+    image(shibaimg, windowWidth*0.2,100,150,150);
     image(bg,0,0,windowWidth, windowWidth);
 }
